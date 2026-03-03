d1dc8a9b27820b7702866eb970688b11d423a36d
Dima Menetro <kinoru11@gmail.com>
2026-03-03 06:18:59 +0000

    File changes

--- src/pages/Processing.jsx
+++ src/pages/Processing.jsx
@@ -3,6 +3,7 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
 import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '../utils';
+import { useAccessory } from '../components/ui/AccessoryContext';
 import { Button } from '@/components/ui/button';
 import { Progress } from '@/components/ui/progress';
 import { 
